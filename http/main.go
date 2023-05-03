e1a1f7be29b8b0af3e4dead3e2481d5749faf0dc
Nicolas Aguiar <nicolas98aguiar@gmail.com>
2023-05-03 15:22:52 -0300

    fix: correção de commits

--- http/main.go
+++ http/main.go
@@ -5,18 +5,30 @@ import (
 	"net/http"
 )
 
+
 func main() {
-		req, err := http.Get("https://www.google.com")
-		if err != nil {
-			panic(err)
-		}
-
-		defer req.Body.Close()
-
-		res, err := io.ReadAll(req.Body)
-		if err != nil {
-			panic(err)
-		}
-
-		println(string(res))
+	http.Handle("/", blog{})
+  http.ListenAndServe(":8080", nil)
 }
+
+type blog struct{}
+
+func (b blog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  io.WriteString(w, "Hello World!")
+}
+
+// func main() {
+// 		req, err := http.Get("https://www.google.com")
+// 		if err != nil {
+// 			panic(err)
+// 		}
+
+// 		defer req.Body.Close()
+
+// 		res, err := io.ReadAll(req.Body)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+
+// 		println(string(res))
+// }
